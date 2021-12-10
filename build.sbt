b1a03e787accef4319d207bd7c728f694341ebfd
Maxence Tallon <maxence.tallon@octo.com>
2021-12-10 09:51:55 +0100

    Day1 Bonus

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 name := "AdventOfCode"
 
 version := "0.1"
+scalaVersion := "2.13.7"
 
 libraryDependencies += "com.github.pureconfig" %% "pureconfig" % "0.13.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % "test"
--- src/main/scala/AdendtOfCode/Day1.scala
+++ src/main/scala/AdendtOfCode/Day1.scala
@@ -5,6 +5,11 @@ object Day1 {
 
   def sign(value :Int) :Int= if (value < 0)  1 else 0
   def compute(input: List[String]) = {
-     input.zip(input.tail).foldLeft(0)((acc,x)=> acc + sign(x._1.toInt -x._2.toInt))
+    input.zip(input.tail).foldLeft(0)((acc, x) => acc + sign(x._1.toInt - x._2.toInt))
+  }
+  def bonus(input: List[String])   ={
+     val res_input = input.zip(input.tail.zip(input.tail.tail)).map(x=> (x._1.toInt,x._2._1.toInt,x._2._2.toInt)).map(x => (x._1+x._2+x._3).toString)
+    compute(res_input)
+  }
 
-}}
+}
